f81392b91747b6100d028a16abaa536dfc72290d
Nicola Vitaly <vitaly@bangunindo.com>
2022-01-13 13:18:39 +0700

    wip

--- main.go
+++ main.go
@@ -1,18 +1,16 @@
 package main
 
 import (
-	"api-fiber/database"
-	"api-fiber/router"
+	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
-	"log"
 )
 
 func main() {
 	app := fiber.New()
 	app.Use(cors.New())
 
-	database.ConnectDB()
-
-	router.SetupRoutes(app)
-	log.Fatal(app.Listen(":3000"))
+	//database.ConnectDB()
+	//
+	//router.SetupRoutes(app)
+	//log.Fatal(app.Listen(":3000"))
 }
